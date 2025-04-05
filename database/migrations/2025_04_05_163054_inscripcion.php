be62522c599a1c1c30412dc225e7ed7980063c01
Leyton Cespedes <leytoncespedes123@gmail.com>
2025-04-05 13:03:14 -0400

    Correcciones

--- database/migrations/2025_04_05_163054_inscripcion.php
+++ database/migrations/2025_04_05_163054_inscripcion.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 class Inscripcion extends Migration
 {
-    public function up(){
+    public function up()
+    {
         Schema::create('inscripcion', function (Blueprint $table) {
             $table->id('idInscripcion');
             $table->date('fechaInscripcion');
@@ -14,19 +15,17 @@ class Inscripcion extends Migration
             $table->unsignedBigInteger('idGrado');
             $table->unsignedBigInteger('idConvocatoria');
             $table->unsignedBigInteger('idArea');
-            $table->unsignedBigInteger('idDelegacion');           
-            $table->foreign('idGrado')->references('idGrado')->on('grado')->onDelete('cascade');
-            $table->foreign('idConvocatoria')->references('idConvocatoria')->on('convocatoria')->onDelete('cascade');
-            $table->foreign('idArea')->references('idArea')->on('area')->onDelete('cascade');
-            $table->foreign('idDelegacion')->references('idDelegacion')->on('delegacion')->onDelete('cascade');
+            $table->unsignedBigInteger('idDelegacion');
+            $table->foreign('idGrado')->references('idGrado')->on('grado');
+            $table->foreign('idConvocatoria')->references('idConvocatoria')->on('convocatoria');
+            $table->foreign('idArea')->references('idArea')->on('area');
+            $table->foreign('idDelegacion')->references('idDelegacion')->on('delegacion');
         });
-        
-    } 
-
+    }
 
     public function down()
     {
         Schema::dropIfExists('inscripcion');
-        
     }
 }
+

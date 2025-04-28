15c7fd79d6868e31038a5c6106f91dcfd08368f2
Orlando Altamirano Vargas <orlandoaltamirano85@gmail.com>
2025-04-28 02:55:35 -0400

    Modelos y Migraciones de BoletaPago, VerificacionInscripcion y BoletaPagoInscripcion

---
+++ database/migrations/2025_04_28_060236_verificacion_inscripcion.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('verificacioninscripcion', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idInscripcion');
+            $table->unsignedBigInteger('idBoleta');
+            $table->boolean('valido')->default(false);
+            $table->string('imagen_comprobante')->nullable();
+            //$table->timestamps();
+
+            // Claves foráneas
+            $table->foreign('idInscripcion')->references('idInscripcion')->on('inscripcion')->onDelete('cascade');
+            $table->foreign('idBoleta')->references('idBoleta')->on('boletapago')->onDelete('cascade');
+            
+            // Índice para optimizar consultas
+            $table->index(['idInscripcion', 'idBoleta']);
+            // Índices para mejorar el rendimiento
+            // $table->index('idInscripcion');
+            // $table->index('idBoleta');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verificacioninscripcion');
+    }
+};
+
+
+

aacad762e5d6f4e41e082c663b3a66a2bd9f3f08
kleber <muruchi2018@gmail.com>
2025-05-14 09:05:33 -0400

    separando sidebar navegacion rigbar footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,10 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/custom/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom/navegation.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom/rigbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom/sidebar.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/themeToggle.js') }}"></script>

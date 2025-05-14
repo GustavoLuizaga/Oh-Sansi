4499bf47df680a0474a5c4ad267349de05842ed4
GustavoLuizaga <luizagamerinogustavo1@gmail.com>
2025-05-14 17:04:27 -0400

    Tabla log

--- app/Http/Controllers/DelegacionController.php
+++ app/Http/Controllers/DelegacionController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Barryvdh\DomPDF\Facade as PDF;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
+use Illuminate\Support\Facades\Auth;
 class DelegacionController extends Controller
 {
     public function index(Request $request)
@@ -78,6 +78,7 @@ class DelegacionController extends Controller
         }
 
         // Insertar en la base de datos
+        DB::statement('SET @current_user_id = ' . Auth::id());
         DB::table('delegacion')->insert([
             'codigo_sie' => $request->codigo_sie,
             'nombre' => $request->nombre,
